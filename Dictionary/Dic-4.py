1ebf94c956d01d16719a5ca27df6f23d61d60d2c
Kazi Redwan <redwan.stu@gmail.com>
2024-03-12 04:54:03 +0600

    Add files via upload

---
+++ Dictionary/Dic-4.py
@@ -0,0 +1,9 @@
+mode_1={'Name':'c++', 'Speed':'Fast'}
+mode_2={'Name':'python', 'Speed':'slow'}
+mode_3={'Name':'java', 'Speed':'medium'}
+
+Modes=[mode_1,mode_2,mode_3]
+
+for mode in Modes:
+    print(mode)
+    
